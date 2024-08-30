1d7881d9d48896d51e5f7437f84662dd76b52102
AugustoMagro <augustovmagro@gmail.com>
2024-08-30 01:29:36 -0300

    MOD: Adding dry code

--- academia/urls.py
+++ academia/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from academia.views import index
+from academia.views import index, contact
 
 urlpatterns = [
     path('', index),
+    path('contact/', contact, name='contact')
 ]
--- academia/views.py
+++ academia/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'academia/index.html')
 
+def contact(request):
+    return render(request, 'academia/contact.html')
+
 # Create your views here.
